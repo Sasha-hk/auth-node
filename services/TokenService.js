aa405f580ebff18561f6868e9abbb6fcaf55d10c
Sasha-hk <sachapos50@gmail.com>
2021-12-23 00:20:54 +0200

    Resolve #7

---
+++ services/TokenService.js
@@ -0,0 +1,32 @@
+const TokenModel = require('../models/TokenModel')
+const bcrypt = require('bcrypt')
+
+
+class TokenService {
+    async generateTokens() {
+
+    }
+
+    async saveRefreshToken() {
+
+    }
+
+    async findRefreshToken() {
+
+    }
+    
+    async removeToken() {
+
+    }
+
+    async validateAccessToken() {
+
+    }
+
+    async validateRefreshToken() {
+
+    }
+}
+
+
+module.exports = new TokenService()
