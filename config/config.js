debdd6513ad758a2c439da1088ff7c8e764c594d
Sasha-hk <sachapos50@gmail.com>
2021-12-22 22:30:53 +0200

    Typo in config.js

--- config/config.js
+++ config/config.js
@@ -1,23 +1,23 @@
 module.exports = {
     "development": {
         "username": process.env.DB_USERNAME,
-        "password": prodess.env.DB_PASSWORD,
-        "database": prodess.env.DB_DATABASE,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_DATABASE,
         "host": process.env.DB_HOST,
-        "dialect": prodess.env.DB_DIALECT
+        "dialect": process.env.DB_DIALECT
     },
     "test": {
         "username": process.env.DB_USERNAME,
-        "password": prodess.env.DB_PASSWORD,
-        "database": prodess.env.DB_DATABASE,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_DATABASE,
         "host": process.env.DB_HOST,
-        "dialect": prodess.env.DB_DIALECT
+        "dialect": process.env.DB_DIALECT
     },
     "production": {
         "username": process.env.DB_USERNAME,
-        "password": prodess.env.DB_PASSWORD,
-        "database": prodess.env.DB_DATABASE,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_DATABASE,
         "host": process.env.DB_HOST,
-        "dialect": prodess.env.DB_DIALECT
+        "dialect": process.env.DB_DIALECT
     }
 }
