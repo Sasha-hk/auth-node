ca0560968c6d7fe98e23231e112b8331ae6d2c85
Sasha-hk <sachapos50@gmail.com>
2021-12-22 15:55:02 +0200

    Base setup

---
+++ config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "development": {
+        "username": process.env.DB_USERNAME,
+        "password": prodess.env.DB_PASSWORD,
+        "database": prodess.env.DB_DATABASE,
+        "host": process.env.DB_HOST,
+        "dialect": prodess.env.DB_DIALECT
+    },
+    "test": {
+        "username": process.env.DB_USERNAME,
+        "password": prodess.env.DB_PASSWORD,
+        "database": prodess.env.DB_DATABASE,
+        "host": process.env.DB_HOST,
+        "dialect": prodess.env.DB_DIALECT
+    },
+    "production": {
+        "username": process.env.DB_USERNAME,
+        "password": prodess.env.DB_PASSWORD,
+        "database": prodess.env.DB_DATABASE,
+        "host": process.env.DB_HOST,
+        "dialect": prodess.env.DB_DIALECT
+    }
+}
