71be072fa2e55a6cea14c6447d1be77e7ba8a28d
Sasha-hk <sachapos50@gmail.com>
2021-12-23 00:20:31 +0200

    Resolve #6

---
+++ services/UserService.js
@@ -0,0 +1,24 @@
+const UserModel = require('../models/UserModel')
+const TokenService = require('./TokenService')
+
+
+class UserService {
+    async signUp() {
+
+    }
+
+    async logIn() {
+
+    }
+
+    async logOut() {
+
+    }
+
+    async refresh() {
+
+    }
+}
+
+
+module.exports = new UserService()
