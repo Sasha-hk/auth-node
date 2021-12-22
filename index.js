1c1e05f9790604cc582814739ccba8d3496d6043
Sasha-hk <sachapos50@gmail.com>
2021-12-22 16:08:03 +0200

    Make base server config

---
+++ index.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const db = require('./db')
+
+const PORT = process.env.PORT || 3000
+const app = express()
+
+// middlewares
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
+
+// routes
+
+
+const start = async () => {
+    try {
+
+        app.listen(PORT, () => {
+            console.log(`Server started...`)
+        })
+    }
+    catch (e) {
+        console.log(e)
+    }
+}
+
+start()
