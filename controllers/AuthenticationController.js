13f72821924d4e1305ed35cec06c91c31c69ac1e
Sasha-hk <sachapos50@gmail.com>
2021-12-23 00:22:02 +0200

    Resolve #5

---
+++ controllers/AuthenticationController.js
@@ -0,0 +1,20 @@
+class AuthenticationController {
+    async signUp(req, res, next) {
+        
+    }
+
+    async logIn(req, res, next) {
+
+    }
+
+    async logOut(req, res, next) {
+
+    }
+
+    async refresh(req, res, next) {
+
+    }
+}
+
+
+module.exports = new AuthenticationController
